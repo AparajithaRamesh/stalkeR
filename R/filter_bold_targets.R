8bd6e82982d8848763bc59091e356cf410ba5e57
AparajithaRamesh <a.ramesh@rug.nl>
2020-08-03 15:41:57 +0200

    filter bold targets entered

---
+++ R/filter_bold_targets.R
@@ -0,0 +1,21 @@
+#' Filter all the boldness targets
+#'
+#' In these pond experiments, boldness targets (4 targets at the top) were kept the same throughout the experiments
+#' The antenna numbers (or `Unit number` in the output file) that corresponds to boldness targets are: 11,12,13,14,31,32,33,35
+#' This function filters only the boldness targets from the given dataset
+#'
+#' @param mydataset Dataframe with the PIT tag data. It should already be filtered according to the time of exploration/boldness experiments
+#'
+#' @return The filtered dataset
+#'
+#' @export
+
+#FUNCTION: filter only exploration targets
+filter_bold_targets<-function(mydataset)
+{
+  bold_targets<-c(11,12,13,14,31,32,33,35)
+
+  mydataset_new<-mydataset%>%
+    filter(Unit.number %in% bold_targets)
+  mydataset_new
+}
---
+++ R/filter_exp_targets.R
@@ -0,0 +1,21 @@
+#' Filter all the exploration targets
+#'
+#' In these pond experiments, exploration targets (5 targets at the bottom) were kept the same throughout the experiments
+#' The antenna numbers (or `Unit number` in the output file) that corresponds to exploration targets are: 21,22,23,24,25,41,42,43,44,45
+#' This function filters only the exploration targets from the given dataset
+#'
+#' @param mydataset Dataframe with the PIT tag data. It should already be filtered according to the time of exploration/boldness experiments
+#'
+#' @return The filtered dataset
+#'
+#' @export
+
+#FUNCTION: filter only exploration targets
+filter_exp_targets<-function(mydataset)
+{
+  exp_targets<-c(21,22,23,24,25,41,42,43,44,45)
+
+  mydataset_new<-mydataset%>%
+    filter(Unit.number %in% exp_targets)
+  mydataset_new
+}
