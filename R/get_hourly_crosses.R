4a2e02c161c9aa2276466ed3889dbd77fd7dc543
AparajithaRamesh <a.ramesh@rug.nl>
2020-08-13 11:27:44 +0200

    added get_hourly crosses for ponds

--- R/get_hourly_crosses.R
+++ R/get_hourly_crosses.R
@@ -17,6 +17,13 @@ get_hourly_crosses<-function(pit_data,
 #filter useful cols and add the date-time column here already
 pit_data<-get_useful_cols(pit_data)
 
+#get the test_details time in posixct format
+test_details$Start_time_pond<-as.POSIXct(test_details$Start_time_pond, tz="UTC")
+test_details$End_time_pond<-as.POSIXct(test_details$End_time_pond, tz="UTC")
+test_details$Start_time_exp<-as.POSIXct(test_details$Start_time_exp, tz="UTC")
+test_details$End_time_exp<-as.POSIXct(test_details$End_time_exp, tz="UTC")
+
+#handle the method
   if (method == "exp"){
   }
 
@@ -33,45 +40,52 @@ pit_data<-get_useful_cols(pit_data)
     for(j in 1:nrow(test_details)){
       #filter according to test time
       my_data<-pit_data%>%
-        dplyr::filter(Actual_time > test_details$Start_time_pond[j] & Actual_time < test_details$End_time_pond[j])
+        dplyr::filter(Actual_time > test_details$Start_time_pond[j]
+                      & Actual_time < test_details$End_time_pond[j])
 
-      #number of hours of test
+      #give pond_id to Unit.number i.e. the antenna number
+      my_data<-my_data%>%
+        dplyr::mutate("pond_id"=dplyr::recode(Unit.number, "44"=1,"43"=2,"42"=2,"41"=3,"35"=3,"33"=4,"32"=4,"31"=5,
+                                              "24" = 1,"23"=2,"22"=2,"21"=3,"14"=3,"13"=4,"12"=4,"11"=5))
+
+      #get the time of crossing wrt to start time
       my_data$crosstime<-as.numeric(my_data$Actual_time-test_details$Start_time_pond[j], units="hours")
 
-  #select particular individual
-  num_indiv<-pure1_new%>%
-    distinct(Transponder.code)
-  total_rows<-nrow(num_indiv)*total_pond_hours #do we need this?? -> yes, to say 0 crosses?
+      #total number of individuals detected in test
+      num_indiv<-my_data%>%
+        dplyr::distinct(Transponder.code)
 
-  #initialize a vector
-  num_indiv$Transponder.code
-  tag_ID<-rep(num_indiv$Transponder.code,round_number_hours) ##dont understand waht this is :/
-  pond_crosses<-c(rep(NA,total_rows))
-  hour<-rep(1:round_number_hours, each = nrow(num_indiv))
-  final_data<-data.frame(tag_ID,hour,pond_crosses)
-
-
-  #loop for time should be here before looping through individuals since we
-  for(j in 1:round_number_hours){
-    filter_time<-pure1_new%>%
-      filter(crossroundtime>j-1)%>%
-      filter(crossroundtime<j)%>%
-      arrange(Identifier)
+  #loop for time in hours of test
+  for(i in 1:total_pond_hours){
+    filter_time<-my_data%>%
+      dplyr::filter(crosstime>i-1)%>%
+      dplyr::filter(crosstime<i)%>%
+      dplyr::arrange(Identifier)
 
     #loop through individuals in the data
-    for(i in 1:nrow(num_indiv)){
+    for(ind in 1:nrow(num_indiv)){
+      #select that particular individual
+      focal_ind<-num_indiv[[1]][ind]
+
       indiv<-filter_time%>%
-        filter(Transponder.code==num_indiv[[1]][i])
-
-      #calculate all the pond changes
-      pond_crosses<-which(indiv$pond_id != dplyr::lag(indiv$pond_id))
-      print (c(num_indiv[[1]][i],length(pond_crosses)))
-
-      final_data$pond_crosses[((j-1)*nrow(num_indiv)+i)]<-length(pond_crosses)
+        dplyr::filter(Transponder.code==focal_ind)
+      #see if that individual is present in this hour or not
+      if(nrow(indiv)==0){
+        pond_crosses<-0
+        new_row<-c(focal_ind, test_details$test_ID[j],as.character(test_details$Start_time_pond[j]),i,pond_crosses)
+      }
+      else{
+        #calculate all the pond changes
+        pond_crosses<-length(which(indiv$pond_id != dplyr::lag(indiv$pond_id)))
+        new_row<-c(focal_ind, test_details$test_ID[j],as.character(test_details$Start_time_pond[j]),i,pond_crosses)
+      }
+      #add new row to empty dataset
+      empty_data<-rbind(empty_data, new_row)
     }
   }
     }
   }
+empty_data
 }
 
 
