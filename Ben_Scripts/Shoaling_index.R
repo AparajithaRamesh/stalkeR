defa35873590c3db19bc3c7681825e24ef4aa51a
BenKawam <ben.kawam@gmail.com>
2020-10-15 15:15:55 +0200

    Reorganised a bit the script

--- Ben_Scripts/Shoaling_index.R
+++ Ben_Scripts/Shoaling_index.R
@@ -44,114 +44,82 @@
   df_list_ant <- split(new_dataset, f = new_dataset$antenna)
 
 # I define my objects
-time.window <- 5 # Time window in seconds
-nb.antennas <- length(df_list_ant) # Number of antennas considered
-individuals <- unique(new_dataset$id) # A character vector containing all the recorded invididuals
-nb.individuals <- length(individuals) # Number of individuals
-
-
-
-# Define my lists
-co.occurent.ind <- list()
-?with
-
-# For loop sorting the data antenna by antenna
-for (i in 1:nb.antennas){
-
-
-  co.occurent.ind[[i]] <- with(df_list_ant[[i]], df_list_ant[[i]][
-        (
-          id == individuals[13] |
-            antenna %in% antenna[id == individuals[13]]
-        ) &
-          apply(
-            sapply(time[id == individuals[13]],
-                   function(x) abs(difftime(time, x, units = "s")) <= time.window ), 1, any
-          )
-        ,]
-      )
-    # Remove the rows with the focal individuals
-    co.occurent.ind[[i]] <-  subset(co.occurent.ind[[i]], id!=individuals[14])
-
-
-    } # end the antenna loop
-
+  time.window <- 5 # Time window in seconds
+  nb.antennas <- length(df_list_ant) # Number of antennas considered
+  individuals <- unique(new_dataset$id) # A character vector containing all the recorded invididuals
+  nb.individuals <- length(individuals) # Number of individuals
 
 
 ############################
 
 
-# Let's scrutinize this code
+## 2. OBTAIN INDIVIDUALS CO-OCCURRING WITH OTHERS WITHIN A CERTAIN TIME WINDOW
 
-# For loop sorting the data antenna by antenna
-#for (i in 1:nb.antennas){
+# Define list.ind. This list will contain dataframes, one per individual. In these dataframes, each row will
+# be one read of a co-occurring individual.
+  list.ind <- list()
 
-  DF3 <-
-      with(
-      df_list_ant[[3]], df_list_ant[[3]][
-      apply(
+# Loop sorting the data individual by individual
+  for (a in 1:13){
 
-      sapply(
-      time[id == "0007A5C9A3"],
-      function(x) abs(difftime (time, x, units = "s")) <= time.window
-      ),
+# Loop sorting the data antenna by antenna
+  for (i in 1:nb.antennas){
 
-      MARGIN = 1, # Function applies to rows
-      any)
-      ,]
-      )
-
-  DF3 <-  subset(DF3, id != "0007A5C9A3")
-  DF3
-
-  ?sapply
-
-
-
-
-
-
-  DF2 <- subset(df_list_ant[[2]], id == "0007A5C9A3")
-
-
-
-
-  a <- for (i in 1:100){
-  subset(df_list_ant[[2]],
-  abs(difftime
-      (time[i], time[i], units = "s")) <= time.window
-  )}
-
-############################
-
-list.ind <- list()
-# For loop sorting the data individual by individual
-for (a in 1:13){
-
-# For loop sorting the data antenna by antenna
-for (i in 1:nb.antennas){
-
-
-  co.occurent.ind[[i]] <- with(df_list_ant[[i]],df_list_ant[[i]][
+# The whole part just below is still confusing to me
+    co.occurent.ind[[i]] <- with(df_list_ant[[i]], df_list_ant[[i]][
     apply(
 
-    sapply(time[id==individuals[a]],
-    function(x) abs(difftime(time,x,units = "s")) <= time.window ),
+    sapply(time[id == individuals[a]],
+    function(x) abs(difftime(time, x, units = "s")) <= time.window ),
 
     1, any)
     ,]
     )
 
-  # Remove the rows with the focal individuals
-  co.occurent.ind[[i]] <-  subset(co.occurent.ind[[i]], id!=individuals[a])
-  list.ind[[a]] <- bind_rows(co.occurent.ind)
+# Remove the rows with the focal individuals. I only keep the individuals co-occuring with the focal fish.
+ co.occurent.ind[[i]] <-  subset(co.occurent.ind[[i]], id != individuals[a])
+# A list of dataframe (one dataframe per individual). Each row = one co-ocurrent individual.
+ list.ind[[a]] <- bind_rows(co.occurent.ind)
+# I indicate for each row which one is the focal individual
+ list.ind[[a]] <- data.frame(list.ind[[a]], Focal.ind = individuals[a])
 
-  # A list of dataframe (one dataframe per individual). Each row = one co-ocurrent individual.
-  list.ind[[a]] <- data.frame(list.ind[[a]], Focal.ind = individuals[a])
+# I obtain the number of individuals recorded with the focal subjects within a certain time window
+ Shoaling.df[a,] <- data.frame(individuals[a], nrow(list.ind[[a]]))
 
-  # I obtain the number of individuals recorded with the focal subjects within a certain time window
-  Shoaling.df[a,] <- data.frame(individuals[a], nrow(list.ind[[a]]))
-} # end the antenna loop
+ } # end the antenna loop
+ } # end of individual loop
 
-} # end of individual loop
 
+
+
+
+############################
+
+# The chunk above isn't really finished. The thing is: not all individuals have been recorded by all antennas.
+# Specifically, ind. 0007A5C9A3 hasn't been recorded by antenna 3 and 4.
+  subset(df_list_ant[[4]], df_list_ant[[4]]$id == "0007A5C9A3") # Gives a 0x4 tibble
+# Therefore, if I run the code above for all individuals and all antennas, it crashes.
+# The chunk of code below enables me to work on a simplified version to try to solve it.
+
+
+  DF <-
+    with(
+      df_list_ant[[4]], df_list_ant[[4]][
+        apply(
+
+            sapply(
+            time[id == "0007A5C9A3"],
+            function(x) abs(difftime (time, x, units = "s")) <= time.window
+            ),
+
+
+          MARGIN = 1, # Function applies to rows
+          any)
+        ,]
+    )
+
+  DF <-  subset(DF3, id != "0007A5C9A3")
+  DF
+
+
+#############################
