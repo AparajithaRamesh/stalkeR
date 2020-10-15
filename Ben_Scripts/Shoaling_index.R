e0cb95726482af3d0baa82f18a4282434f2fceb7
BenKawam <ben.kawam@gmail.com>
2020-10-15 14:28:22 +0200

    Shorter version of the code. I still have a problem if certain individuals haven't been read by certain antennas.

--- Ben_Scripts/Shoaling_index.R
+++ Ben_Scripts/Shoaling_index.R
@@ -36,35 +36,37 @@
   df$Transponder.code <- as.character(df$Transponder.code)
   df$Actual_time <- dmy_hms(paste(df$Date,df$Time,sep=" "))
 
-  # I make a new df with a subset of the variables of interest here
+# I make a new df with a subset of the variables of interest here
   new_dataset<-subset(df, select=c(Identifier, Actual_time, Unit.number, Transponder.code))
   names(new_dataset) <- c("Identifier", "time", "antenna", "id")
 
-
+# A list of dataframe. Each dataframe corresponds to one antenna.
   df_list_ant <- split(new_dataset, f = new_dataset$antenna)
 
+# I define my objects
 time.window <- 5 # Time window in seconds
 nb.antennas <- length(df_list_ant) # Number of antennas considered
-individuals <- unique(new_dataset$id)
+individuals <- unique(new_dataset$id) # A character vector containing all the recorded invididuals
 nb.individuals <- length(individuals) # Number of individuals
 
 
 
-
+# Define my lists
 co.occurent.ind <- list()
+?with
 
 # For loop sorting the data antenna by antenna
 for (i in 1:nb.antennas){
 
 
-  co.occurent.ind[[i]] <- with(df_list_ant[[i]],df_list_ant[[i]][
+  co.occurent.ind[[i]] <- with(df_list_ant[[i]], df_list_ant[[i]][
         (
-          id==individuals[14] |  #individuals[i]
-            antenna %in% antenna[id==individuals[14]]
+          id == individuals[13] |
+            antenna %in% antenna[id == individuals[13]]
         ) &
           apply(
-            sapply(time[id==individuals[14]],
-                   function(x) abs(difftime(time,x,units="s")) <= time.window ), 1, any
+            sapply(time[id == individuals[13]],
+                   function(x) abs(difftime(time, x, units = "s")) <= time.window ), 1, any
           )
         ,]
       )
@@ -74,7 +76,51 @@ for (i in 1:nb.antennas){
 
     } # end the antenna loop
 
+
+
 ############################
+
+
+# Let's scrutinize this code
+
+# For loop sorting the data antenna by antenna
+#for (i in 1:nb.antennas){
+
+  DF3 <-
+      with(
+      df_list_ant[[3]], df_list_ant[[3]][
+      apply(
+
+      sapply(
+      time[id == "0007A5C9A3"],
+      function(x) abs(difftime (time, x, units = "s")) <= time.window
+      ),
+
+      MARGIN = 1, # Function applies to rows
+      any)
+      ,]
+      )
+
+  DF3 <-  subset(DF3, id != "0007A5C9A3")
+  DF3
+
+  ?sapply
+
+
+
+
+
+
+  DF2 <- subset(df_list_ant[[2]], id == "0007A5C9A3")
+
+
+
+
+  a <- for (i in 1:100){
+  subset(df_list_ant[[2]],
+  abs(difftime
+      (time[i], time[i], units = "s")) <= time.window
+  )}
 
 ############################
 
@@ -87,20 +133,24 @@ for (i in 1:nb.antennas){
 
 
   co.occurent.ind[[i]] <- with(df_list_ant[[i]],df_list_ant[[i]][
-    (
-      id==individuals[a] |
-        antenna %in% antenna[id==individuals[a]]
-    ) &
-      apply(
-        sapply(time[id==individuals[a]],
-               function(x) abs(difftime(time,x,units="s")) <= time.window ), 1, any
-      )
+    apply(
+
+    sapply(time[id==individuals[a]],
+    function(x) abs(difftime(time,x,units = "s")) <= time.window ),
+
+    1, any)
     ,]
-  )
+    )
+
   # Remove the rows with the focal individuals
   co.occurent.ind[[i]] <-  subset(co.occurent.ind[[i]], id!=individuals[a])
   list.ind[[a]] <- bind_rows(co.occurent.ind)
 
+  # A list of dataframe (one dataframe per individual). Each row = one co-ocurrent individual.
+  list.ind[[a]] <- data.frame(list.ind[[a]], Focal.ind = individuals[a])
+
+  # I obtain the number of individuals recorded with the focal subjects within a certain time window
+  Shoaling.df[a,] <- data.frame(individuals[a], nrow(list.ind[[a]]))
 } # end the antenna loop
 
 } # end of individual loop
