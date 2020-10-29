b710a9b315cb9fbd7b57652c98227cad468a612d
BenKawam <ben.kawam@gmail.com>
2020-10-29 19:07:47 +0100

    I modified this script so it could have as an input a raw dataframe from one day. Output has some information about pond number, replicate. Also I have some violins at the end

--- Ben_Scripts/Travelled_distance.R
+++ Ben_Scripts/Travelled_distance.R
@@ -1,5 +1,4 @@
-# Here I obtain the number of antenna changes as well as distance travelled between antennas per individual for one pond
-# where the antennas S1-S2-S3-S4 are 11-12-13-14.
+# Here I obtain the number of antenna changes as well as distance travelled between antennas per individual.
 
 
   # Packages
@@ -16,17 +15,41 @@
 
 ## 1. DATA IMPORT AND DEFINING MY OBJECTS
     # Import data
-    df <- read_delim("20201008.CSV", ";", escape_double = FALSE, trim_ws = TRUE)
+    df1 <-  read_delim("~/Cours/M2 - Sticklebacks/Data/1. Preliminary tests/4. Week -1/Oct 27/20201027.CSV", 
+                       ";", escape_double = FALSE, trim_ws = TRUE)
+    df2 <- read_delim("~/Cours/M2 - Sticklebacks/Data/1. Preliminary tests/4. Week -1/Oct 28/20201028.CSV", 
+                      ";", escape_double = FALSE, trim_ws = TRUE)
+    
+    df <- rbind(df1, df2)
 
     # Distances between antennas
     pond.width   <- 1
     pond.length  <- 1.1
-    pond.diagonal<- 1.25
+    pond.diagonal<- 1.5
 
     # Sequence of antenna changes corresponding to the distances above
-    seq.width  <- c('1112', '1211', '1314', '1413')
-    seq.length <- c('1113', '1311', '1214', '1412')
-    seq.diag   <- c('1114', '1411', '1213', '1312')
+    
+                #######################
+                #                     # 
+                #        #   #        #
+                #     #        #      #
+                #   #  3      4   #   #
+                #   #             #   #
+                #   #             #   #
+                #   #             #   #
+                #   #  2      1   #   #
+                #     #        #      #
+                #       ## ##         #
+                #                     #
+                #    Central pond     #
+                #         below       #
+                #                     #
+                #######################
+    
+    
+    seq.width  <- c('12', '21', '34', '43')
+    seq.length <- c('13', '31', '24', '42')
+    seq.diag   <- c('14', '41', '23', '32')
 
 
 ## 2. DATA MANIPULATION
@@ -49,16 +72,41 @@
     new_dataset<-subset(df, select=c(Identifier, Actual_time, Unit.number, Transponder.code))
     names(new_dataset) <- c("Identifier", "time", "antenna", "id")
 
-    trav.dist <- function(new_dataset,
-                          pond.width, pond.length, pond.diagonal,
-                          seq.width, seq.length, seq.diag){
+    # I rename my antennas with a standardised notation across ponds (see scheme above)
+    new_dataset[new_dataset == 11 | new_dataset == 21 | new_dataset == 31 | new_dataset == 41] <- 1
+    new_dataset[new_dataset == 12 | new_dataset == 22 | new_dataset == 32 | new_dataset == 42] <- 2
+    new_dataset[new_dataset == 13 | new_dataset == 23 | new_dataset == 33 | new_dataset == 43] <- 3
+    new_dataset[new_dataset == 14 | new_dataset == 24 | new_dataset == 35 | new_dataset == 44] <- 4
+    
+    
+    
+    
+    
+    # Now, I will remove all the reads that do not come from the fish present in the ponds (e.g. ghost reads, test reads)
+    # I import my list of fish
+    list_fish <- read_excel("~/Cours/M2 - Sticklebacks/Data/1. Preliminary tests/4. Week -1/Ben_PITtag_final_grouped_FINAL.xlsx", 
+                            sheet = "Sheet1")
+
+    # I identify the reads that do not  come from the ponds
+    ghost_reads <- setdiff(new_dataset$id, list_fish$id)
+
+    # I remove all of them from the dataframe
+    for (i in 1:length(ghost_reads)){
+    new_dataset <- new_dataset[!(new_dataset$id == ghost_reads[i]),]
+    }
+    
+    # Now, if I run this line, I should obtain a value equal or smaller than 200.
+    length(unique(new_dataset$id))
+    
+    
+    
+    trav.dist <- function(new_dataset){
     
     # I split my dataframe into a list of dataframes (one object per individual)
     df_list <- split(new_dataset, f = new_dataset$id)
 
 
-
-
+    
 ## 3. REMOVE THE REPEATED READS AND OBTAIN THE ANTENNA CHANGE
     # For each individual, I reduce the input vector (e.g., 1, 1, 1, 2, 2, 3) in a
     # way that one read is kept per sequence of identical numbers (e.g., 1, 2, 3).
@@ -86,12 +134,12 @@
 
     # I obtain the number of times each individual was read in two different antennas consecutively
     Changes <- sapply(df_list_red, nrow)
-    Names <- numeric() # I obtain the individual names
+    id <- numeric() # I obtain the individual names
     for(i in 1:length(df_list_red)){
-      Names[i] <- df_list_red[[i]][1,3]}
+      id[i] <- df_list_red[[i]][1,3]}
 
     # I can generate a data frame with number of changes per individual
-    df3 <- data.frame(Names, Changes)
+    df3 <- data.frame(id, Changes)
 
 
 ## 4. DISTANCE TRAVELLED BY EACH INDIVIDUAL
@@ -118,17 +166,20 @@
 
     # Total distance travalled by all individuals
     df4 <- cbind(df3, Dist)
+    
     return(df4)
     }
 
   # I run the 'trav.dist' function
-    Travelled.distance <- trav.dist(df,
-             pond.width, pond.length, pond.diagonal,
-             seq.width, seq.length, seq.diag)
+    Travelled.distance <- trav.dist(new_dataset)
+    
+    # I assign every individual to its pond and treatment category
+    Travelled.distance <- merge(Travelled.distance, list_fish[c(2,8,9, 10)], by = "id")
 
+  
     # I check if individuals might have not been recorded at all by the antennas
-    non_read_babies <- setdiff(df$`Transponder code`, Travelled.distance$Names)
-    non_read_babies <- data.frame(Names = non_read_babies, Changes =  0, Dist =  0)
+    non_read_babies <- setdiff(df$`Transponder code`, Travelled.distance$id)
+    non_read_babies <- data.frame(id = non_read_babies, Changes =  0, Dist =  0)
 
     # Final df containing the read (and potential non-read) individuals
     Travelled.distance <- rbind(Travelled.distance, non_read_babies)
@@ -137,5 +188,12 @@
 
     # We can see that as expected, the correlation between nb of crosses and distance is strong
     plot(Travelled.distance$Changes, Travelled.distance$Dist)
-
-
+    
+    
+    ggplot(data = Travelled.distance, aes(x = Treatment_seq, y = Dist, fill = Replicate)) + 
+      geom_violin(position = position_dodge(width = 0.4)) + 
+      geom_boxplot(width=.1, outlier.colour=NA, position = position_dodge(width = 0.4)) +
+      scale_fill_brewer(palette="Blues") + theme_classic() 
+      #geom_dotplot(binaxis='y', stackdir='center', dotsize=0.5, 
+      #             position=position_dodge(0.8), alpha = 0.5, fill = replicate)
+   
