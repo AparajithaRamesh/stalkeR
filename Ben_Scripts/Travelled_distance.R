ccb8c9f1468f84b7a91e95ae240247d99eeae68b
BenKawam <ben.kawam@gmail.com>
2020-10-31 10:59:29 +0100

    A second possible path for the plotting is with 'geom_split violin'. I added the function at the end of the script.

--- Ben_Scripts/Travelled_distance.R
+++ Ben_Scripts/Travelled_distance.R
@@ -281,16 +281,84 @@
       geom_line(data = sumrepdat, aes(y =  Dist_mean, group = Treatment_seq, 
                                       colour = Treatment_seq), linetype = 1, size = 1) +
  
-      geom_errorbar(data = sumrepdat, aes( 
-        y =  Dist_mean, group = Treatment_seq, 
-        colour = Treatment_seq, ymin =  Dist_mean-se, 
-        ymax =  Dist_mean+se), width = 0, size = 1.2) +
+      #geom_errorbar(data = sumrepdat, aes( 
+      #  y =  Dist_mean, group = Treatment_seq, 
+      #  colour = Treatment_seq, ymin =  Dist_mean-sd, 
+      #  ymax =  Dist_mean+se), width = 0.05, size = 1.2) +
+      
       geom_point(data = sumrepdat, aes(y =  Dist_mean, group = Treatment_seq, 
                                        colour = Treatment_seq), shape = 19, size = 2)
       
 
-    ?geom_line()
 
     
+    # SPLIT VIOLIN
+
+    
+    ggplot(Travelled.distance2, aes(x = time, y = Dist, fill = Treatment_seq))+
+      geom_point(aes(x = time, y = Dist, colour = Treatment_seq), 
+                 position = position_dodge(width = .5), 
+                 size = 1, shape = 19, alpha = 0.4) +
+      
+      geom_split_violin(aes(colour = Treatment_seq), trim = FALSE, alpha = .5) 
+      
+      
+
+    
+    
+    
+    # geom_split_violin function
+    GeomSplitViolin <- ggproto(
+      "GeomSplitViolin", 
+      GeomViolin, 
+      draw_group = function(self, data, ..., draw_quantiles = NULL) {
+        data <- transform(data, 
+                          xminv = x - violinwidth * (x - xmin), 
+                          xmaxv = x + violinwidth * (xmax - x))
+        grp <- data[1,'group']
+        newdata <- plyr::arrange(
+          transform(data, x = if(grp%%2==1) xminv else xmaxv), 
+          if(grp%%2==1) y else -y
+        )
+        newdata <- rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
+        newdata[c(1,nrow(newdata)-1,nrow(newdata)), 'x'] <- round(newdata[1, 'x']) 
+        if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
+          stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <= 1))
+          quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
+          aesthetics <- data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
+          aesthetics$alpha <- rep(1, nrow(quantiles))
+          both <- cbind(quantiles, aesthetics)
+          quantile_grob <- GeomPath$draw_panel(both, ...)
+          ggplot2:::ggname("geom_split_violin", 
+                           grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
+        } else {
+          ggplot2:::ggname("geom_split_violin", GeomPolygon$draw_panel(newdata, ...))
+        }
+      }
+    )
+    
+    geom_split_violin <- function (mapping = NULL, 
+                                   data = NULL, 
+                                   stat = "ydensity", 
+                                   position = "identity", ..., 
+                                   draw_quantiles = NULL, 
+                                   trim = TRUE, 
+                                   scale = "area", 
+                                   na.rm = FALSE, 
+                                   show.legend = NA, 
+                                   inherit.aes = TRUE) {
+      layer(data = data, 
+            mapping = mapping, 
+            stat = stat, 
+            geom = GeomSplitViolin, 
+            position = position, 
+            show.legend = show.legend, 
+            inherit.aes = inherit.aes, 
+            params = list(trim = trim, 
+                          scale = scale, 
+                          draw_quantiles = draw_quantiles, 
+                          na.rm = na.rm, ...)
+      )
+    }
       
     
