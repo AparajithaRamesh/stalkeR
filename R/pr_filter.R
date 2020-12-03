01c164847f4fd25b24ef3ad1c9e11abe1cff34b7
BenKawam <ben.kawam@gmail.com>
2020-12-03 10:27:59 +0100

    Documented the function

--- R/pr_filter.R
+++ R/pr_filter.R
@@ -1,42 +1,70 @@
-#' Title
+#'  pr_filter
 #'
-#' @param clean_df
-#' @param block_nb
-#' @param ind_names
-#' @param start_time
-#' @param end_time
+#' @description This function subsets a clean (and potentially large) data frame based on four different possible parameters:
+#' experimental block number/name, individuals id, start time and end time.
+#'
+#' These parameters are cumulative. If for instance, I chose experimental block 7 with "2020-11-05 12:30:00" as
+#' starting time, it will take the reads, within the experimental block 7,
+#' that occurred after 2020-11-05 12:30:00.
+#'
+#'
+#' @param clean_df A clean data frame of any size, containing at least one experimental blocks, and potentially many.
+#'
+#' @param block_nb The number or name of an experimental block (e.g. enclosure, pond, lake).
+#'
+#' @param ind_names A vector containing the names of all the individuals to subset.
+#'
+#' @param start_time The time from which the experimental block starts (POSIXct format, see examples).
+#'
+#' @param end_time The time until which the experimental block lasts (POSIXct format, see examples).
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#'
+#' # Define inputs
+#' ind_names_1 <- c("0007E50321", "0007A34978", "0007DF1B76")
+#' ind_names_2 <- c("Apu", "Jakob", "Franjo")
+#' pond_nb <- 7
+#' cage_nb <- cage5
+#' start_time_5_nov <- as.POSIXct(strptime(c("2020-11-05 12:30:00"),"%Y-%m-%d %H:%M:%OS"),"UTC")
+#' end_time_5_nov <- as.POSIXct(strptime(c("2020-11-05 14:00:00"),"%Y-%m-%d %H:%M:%OS"),"UTC")
+#'
+#' # Run the function with different parameters
+#' pr_filter(clean_df, ind_names = ind_names_1)
+#' pr_filter(clean_df, block_nb = pond_nb)
+#' pr_filter(clean_df, start_time = start_time)
+#' pr_filter(clean_df, start_time_5_nov = start_time_5_nov)
+#' pr_filter(clean_df, ind_names = ind_names_2, block_nb = cage5, end_time = end_time_5_nov)
+#'
 pr_filter <- function(clean_df, block_nb, ind_names, start_time, end_time){
 
   # If given individuals names
   if(!missing(block_nb)) {
     ind_names <- subset(id_ref_df$id, id_ref_df$Pond == block_nb)
-    a <- clean_df[clean_df$id %in% ind_names,]
+    clean_df <- clean_df[clean_df$id %in% ind_names,]
   }
 
-  else
+
   # If given pond number
   if(!missing(ind_names)) {
-    a <- clean_df[clean_df$id %in% ind_names,]
+    clean_df <- clean_df[clean_df$id %in% ind_names,]
   }
 
-  else
+
   # If given start time
   if(!missing(start_time)) {
-    a <- subset(clean_df, start_time < time)
+    clean_df <- subset(clean_df, start_time < time)
   }
 
-  else
+
   # If given end time
   if(!missing(end_time)) {
-    a <- subset(clean_df, end_time > time)
+    clean_df <- subset(clean_df, end_time > time)
   }
 
-  return(a)
+  return(clean_df)
 }
 
 
@@ -49,11 +77,16 @@ pr_filter <- function(clean_df, block_nb, ind_names, start_time, end_time){
 #ind_names <- subset(id_ref_df$id, id_ref_df$Pond == 7)
 #block_nb <- 7
 #start_time <- as.POSIXct(strptime(c("2020-11-05 12:30:00"),"%Y-%m-%d %H:%M:%OS"),"UTC")
-#end_time <- as.POSIXct(strptime(c("2020-11-05 15:00:00"),"%Y-%m-%d %H:%M:%OS"),"UTC")
+#end_time <- as.POSIXct(strptime(c("2020-11-05 14:00:00"),"%Y-%m-%d %H:%M:%OS"),"UTC")
 
 
 #pr_filter(clean_df, ind_names = ind_names)
 #pr_filter(clean_df, block_nb = 7)
 #pr_filter(clean_df, start_time = start_time)
 #pr_filter(clean_df, end_time = end_time)
-#pr_filter(clean_df, ind_names = ind_names, block_nb = 7, start_time = start_time)
+#pr_filter(clean_df, ind_names = ind_names, block_nb = 7, end_time = end_time)
+#
+# roxygen2::roxygenise()
+# ?pr_filter
+
+
