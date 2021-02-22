f04eabcff61336ea497a022652d2c546179a3edf
AparajithaRamesh <a.ramesh@rug.nl>
2021-02-22 16:04:41 +0100

    getting rid of errors and warnings

--- R/pr_clean_data.R
+++ R/pr_clean_data.R
@@ -4,7 +4,7 @@
 #' already formatted and containing the columns
 #' "time", "date", "antenna", "id"  (already formatted). This data frame corresponds to
 #' the reads for \emph{one} experimental block.
-#' @param id_ref_df Reference data frame for an experimental block
+#' @param block_ref_df Reference data frame for an experimental block
 #' containing at least one column "id", with  all
 #' individuals and corresponding variables (already formatted)
 #'
@@ -23,7 +23,7 @@
 #'
 #' @export
 #'
-pr_clean_data <- function(raw_df, id_ref_df) {
+pr_clean_data <- function(raw_df, block_ref_df) {
   # Defining my variables
   raw_df$time <- as.character(raw_df$time)
   raw_df$antenna <- as.integer(raw_df$antenna)
@@ -41,7 +41,7 @@ pr_clean_data <- function(raw_df, id_ref_df) {
 
   # I remove all the reads that do not come from the animals present in the focal experimental
   # block (e.g. ghost reads, test reads). I first identify them:
-  ghost_reads <- setdiff(block_df$id, id_ref_df$id)
+  ghost_reads <- setdiff(block_df$id, block_ref_df$id)
 
   # Then, I remove all of them from the data frame
 
@@ -63,6 +63,6 @@ pr_clean_data <- function(raw_df, id_ref_df) {
 
 
 #raw_df <- readr::read_delim("Dummy data/raw_df.CSV", ";", escape_double = FALSE, trim_ws = TRUE)
-#id_ref_df <- readxl::read_excel("Dummy data/id_ref_df.xlsx", sheet = "Sheet1")
+#block_ref_df <- readxl::read_excel("Dummy data/block_ref_df.xlsx", sheet = "Sheet1")
 
-#pr_clean_data(raw_df, id_ref_df)
+#pr_clean_data(raw_df, block_ref_df)
