4a03d4562610f905cef993a4ed06188e30761183
BenKawam <ben.kawam@gmail.com>
2021-02-22 21:25:40 +0100

    Added a few sections of the Vignette.

--- R/pr_latency_reach.R
+++ R/pr_latency_reach.R
@@ -13,11 +13,11 @@
 #' @param block_ref_df A data frame containing a reference list with all individuals present in the experimental block.
 #'
 #' @param antenna_nb If there is one antenna of interest, then antenna_nb corresponds to its number/name (\code{antenna_nb_1} in examples).
-#' However, if there are several antennas of interest, the argument should be av vector of antenna names/numbers (\code{antenna_nb_2} in examples).
+#' However, if there are several antennas of interest, the argument should be a vector of antenna names/numbers (\code{antenna_nb_2} in examples).
 #'
-#' @param start_time The time from which the experimental block starts (\emph{i.e.POSIXct} format, see examples).
+#' @param start_time The time from which the experimental block starts (\emph{i.e. POSIXct} format, see examples).
 #'
-#' @param end_time The time until which the experimental block lasts (\emph{i.e.POSIXct} format, see examples).
+#' @param end_time The time until which the experimental block lasts (\emph{i.e. POSIXct} format, see examples).
 #'
 #' @param keep_NA Logical argument. If the individuals are never read by the antenna(s), the latency might be set as the maximal possible value,
 #' \emph{i.e.} \code{end_time} - \code{start_time} (default option), or be kept as NA.
