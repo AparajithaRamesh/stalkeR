5ac78f778fa66d73b88745050e377aee047bb7d8
AparajithaRamesh <a.ramesh@rug.nl>
2021-03-05 17:28:41 +0100

    added proper docs for raw_data

--- R/block_ref_df.R
+++ R/block_ref_df.R
@@ -1,14 +1,20 @@
 #' block_ref_df frame obtained from the RFID / PIT tag readers
 #'
-#' A dataset containing the putput with required formatted columns
+#'  This dataframe is the working unit of this package.
+#'  It uses the block_ref_df and raw_df objects as inputs, and outputs a cleaner version of raw_df named block_df. The function does the following actions:
+#'  It keeps the columns of interest (i.e. id, antenna and time).
+#'  The time format is changed to Posixt format.
+#'  It keeps one read per individual at a certain antenna per second (i.e. remove repeats).
+#'  It removes ghost reads test reads, or any read that do not correspond to individuals present in the block_ref_df object.
+#'  It orders the data frame based on time.
+#'  The output block_df, is the working unit of this package.A dataset containing the putput with required formatted columns
 #' "id", "time", "date", "antenna".
 #'
 #' @format A data frame with xxx rows and yy variables:
 #' \describe{
-#'   \item{id}{RFID /PIT tag id of individuals}
-#'   \item{Date}{date of read}
-#'   \item{SL}{time of read}
-#'   \item{Weight}{antenna number}
+#'   \item{id}{Unique RFID / PIT tag id per individuals, usually an alphanumeric}
+#'   \item{SL}{Standard length, a measure of size in fish}
+#'   \item{Weight}{Weight of the fish}
 #'   \item{Plates}{Lateral plates}
 #'   \item{Tagger}{Jakob}
 #'   \item{Remarks}{Remarks while tagging}
--- R/raw_df.R
+++ R/raw_df.R
@@ -1,31 +1,17 @@
 #' Raw data frame obtained from the RFID / PIT tag readers
 #'
-#' A dataset containing the putput with required formatted columns
-#' "id", "time", "date", "antenna".
+#' Raw data frame containing reads from one experimental block, as obtained
+#' from the RFID / PIT transponder. Check vignette for explanation of experimental
+#' block.
 #'
 #' @format A data frame with 808 rows and yy variables:
 #' \describe{
-#'   \item{id}{RFID /PIT tag id of individuals}
-#'   \item{date}{date of read}
-#'   \item{time}{time of read}
-#'   \item{antenna}{antenna number}
-#'   \item{Antenna.number}{time of read}
-#'   \item{Event}{time of read}
-#'   \item{GPS.coordinates}{time of read}
-#'   \item{Identifier}{time of read}
-#'   \item{Input.status}{time of read}
-#'   \item{Output.status}{time of read}
-#'   \item{Transponder.type}{time of read}
-#'   \item{Weight.x}{time of read}
-#'   \item{Date}{Date from the reader directly}
-#'   \item{SL}{Standard length of the fish}
-#'   \item{Weight.y}{Weight of the fish}
-#'   \item{Plates}{Plate category: Completely plated (T), Semi-plated (S) and Low plaetd (L)}
-#'   \item{Pond}{Pond number in which fish are placed}
-#'   \item{Treatment_seq}{Sequence of treatements the fish were subjected to}
-#'   \item{Replicate}{pond replicate}
-#'   \item{Remarks}{Remarks made by the tagger}
-#'   \item{Tagger}{Person who tagged}
+#'   \item{Identifier}{Identifier records the serial number of each read from the RFID / PIT transponder}
+#'   \item{id}{Unique RFID / PIT tag id per individuals, usually an alphanumeric}
+#'   \item{date}{Date of recorded read in dd-mm-yy format}
+#'   \item{time}{Time of recorded read hh:mm:ss format}
+#'   \item{antenna}{Unique antenna number / code where the RFID / PIT tag was read}
+#'   \item{GPS.coordinates}{Optional columns: GPS coordinates of the antenna}
 #'
 #'   ...
 #' }
